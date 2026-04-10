bd56d8dd835c669c24fea882810f312f1a76468f
PC XUAN DIAU <hbui8676@gmail.com>
2026-04-11 01:23:27 +0700

    ViewTaoLichTrinh

---
+++ FE/src/router/index.js
@@ -0,0 +1,20 @@
+import { createRouter, createWebHistory } from "vue-router"; // cài vue-router: npm install vue-router@next --save
+
+const routes = [
+    {
+        path : '/',
+        component: ()=>import('../layout/wrapper/index.vue')
+    },
+    {
+    path: "/tao-lich-trinh",
+    component: () => import("../pages/Client/TaoLichTrinh.vue"),
+    meta: { layout: "client" },
+  },
+]
+
+const router = createRouter({
+    history: createWebHistory(),
+    routes: routes
+})
+
+export default router
