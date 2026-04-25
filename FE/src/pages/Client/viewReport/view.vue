13a5d825053256158d46df26fd8b70928e74790e
htisv <vokhaihoan07112004@gmail.com>
2026-04-25 16:19:41 +0700

    report_hoan

---
+++ FE/src/pages/Client/viewReport/view.vue
@@ -0,0 +1,12 @@
+<template>
+  <div class="view-report">
+    <h2>Xem báo cáo</h2>
+    <p>Trang đang được phát triển...</p>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'ViewReport'
+}
+</script>
